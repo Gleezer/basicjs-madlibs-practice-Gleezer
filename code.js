d4e85d5d568e0296a97397ac7321d64ad2412af1
Cole Craft <colecraft.cc@gmail.com>
2021-03-26 12:25:22 -0400

    Updated with readabilty

--- code.js
+++ code.js
@@ -11,17 +11,32 @@ let food = prompt("Enter your favorite food:")
 
 document.write("Here is my short story!<br>");
 document.write(
-  "Once there was a " +
-    royalty +
-    " who decided to take a walk to the " +
-    place +
-    ". It was a " +
-    weather +
-    " day as the " + royalty + " set out for the " + place + 
-    ". However, it wasnt long before they realized they were hungry for " + food + 
-    ". As they made their way to the " + food + " shop, they came across a " + creature +
-    ". The " + creature +" begged for some food from the kind " + royalty + 
-    ". The " + royalty + " happily shared their " + food + " with the " + creature +
-    ". Then they all lived happily ever after, or something like that."
+   "<br> Once there was a " 
+   + royalty 
+   + " who decided to take a walk to the " 
+   + place 
+   + ". It was a "
+   + weather 
+   + " day as the " 
+   + royalty
+   + " set out for the "
+   +  place 
+   + ". However, it wasnt long before they realized they were hungry for "
+   + food
+   + ". As they made their way to the " 
+   + food 
+   + " shop, they came across a "
+   + creature 
+   + ". The " 
+   + creature 
+   +" begged for some food from the kind " 
+   + royalty 
+   + ". The " 
+   + royalty 
+   + " happily shared their " 
+   + food 
+   + " with the " 
+   + creature 
+   + ". Then they all lived happily ever after, or something like that."
     
 );

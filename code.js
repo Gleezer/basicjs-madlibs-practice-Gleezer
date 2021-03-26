87e56f38d4c954afafb4ab371e9f8dfc494b8b39
Cole Craft <colecraft.cc@gmail.com>
2021-03-26 11:52:36 -0400

    Silly Madlib Completed

--- code.js
+++ code.js
@@ -3,16 +3,24 @@
 
 // Create your own Madlibs story!
 
-let animal = prompt("Name a kind of animal");
-let place = prompt("Name a place");
-let food = prompt("Name a food");
-document.write("Here is a story for you!<br>");
+let royalty = prompt("Enter a type of Royalty such as a Princess or King:");
+let creature = prompt("Enter any type of creature:");
+let weather = prompt("Enter a type of weather:");
+let place = prompt("Enter a type of place:")
+let food = prompt("Enter your favorite food:")
+
+document.write("Here is my short story!<br>");
 document.write(
-  "The inconsiderate " +
-    animal +
-    " strode proudly through " +
+  "Once there was a " +
+    royalty +
+    " who decided to take a walk to the " +
     place +
-    ", dropping " +
-    food +
-    " everywhere."
+    ". It was a " +
+    weather +
+    " day as the " + royalty + " set out for the " + place + 
+    ". However it wasnt long before they realized they were hungry for " + food + 
+    ". As they made their way to the " + food + " shop, they came across a " + creature +
+    ". The " + creature +" begged for some food from the kind " + royalty + 
+    ". The " + royalty + " happily shared their " + food + " with the " + creature +
+    ". Then they all lived happily ever after, or something like that."
 );
